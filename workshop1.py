2ccd0d6f63b80c0dbbe47e3323a613b37e771e4e
chrillesaaf <chrillesaaf@gmail.com>
2025-09-30 15:22:52 +0200

    Printing site locations

--- workshop1.py
+++ workshop1.py
@@ -1,1 +1,8 @@
-print ('Hello World!')
+# Import the json library so that we can handle json
+import json
+
+data = json.load(open("network_devices.json","r",encoding = "utf-8"))
+
+for location in data["locations"]:
+    
+    print(location["site"])
