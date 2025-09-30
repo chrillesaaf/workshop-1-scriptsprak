8bc2ed33e4ba81aae1a16fe2d7fc7463790a2071
chrillesaaf <chrillesaaf@gmail.com>
2025-09-30 16:06:21 +0200

    Update workshop1.py

--- workshop1.py
+++ workshop1.py
@@ -3,6 +3,15 @@ import json
 
 data = json.load(open("network_devices.json","r",encoding = "utf-8"))
 
+# Create a variable that holds our whole text report
+report = ""
+
+# loop through the location list
 for location in data["locations"]:
+    # print the site/name of the location
+    report += location["site"] + "\n"
     
     print(location["site"])
+
+with open('report.txt', 'w', encoding='utf-8') as f:
+    f.write("Rubrik\n")
