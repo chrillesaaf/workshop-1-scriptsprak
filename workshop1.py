d0eabccaae0f34c6df1827ced5931aa0fd314824
chrillesaaf <chrillesaaf@gmail.com>
2025-10-02 12:48:26 +0200

    Offline devices

--- workshop1.py
+++ workshop1.py
@@ -7,14 +7,22 @@ data = json.load(open("network_devices.json","r",encoding = "utf-8"))
 # Create a variable that holds our whole text report
 report = ""
 
-# loop through the location list 
+# Create a summary of the critical data
+summary =""
+summary += "SUMMARY:\n"
+
+offline = 0
 for location in data["locations"]:
-    # add the site/'name' of the location to the report
-    report += "\n" + location["site"] + "\n"
-    # add a list of the host names of the devices 
-    # on the location to the report
     for device in location["devices"]:
-      report += "  " + device["hostname"] + "\n"
+        if device["status"] == "offline":
+            offline += 1
+
+summary += f"⚠ Critical: {offline} devices offline\n"
+
+summary += "⚠ Warning:\n" 
+
+# Add summary before main report
+report = summary + report
 
 # write the report to text file
 with open('report.txt', 'w', encoding='utf-8') as f:
