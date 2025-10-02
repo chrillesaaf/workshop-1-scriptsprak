3bcc0218118ab891cc47af93d77f828b7a49db55
chrillesaaf <chrillesaaf@gmail.com>
2025-10-02 15:22:16 +0200

    251002 ver1

--- workshop1.py
+++ workshop1.py
@@ -7,9 +7,30 @@ data = json.load(open("network_devices.json","r",encoding = "utf-8"))
 # Create a variable that holds our whole text report
 report = ""
 
+report += "\nDEVICES WITH PROBLEMS\n"
+report += "----------------------\n"
+report += "Status: OFFLINE\n"
+
+for location in data["locations"]:
+    for device in location["devices"]:
+        if device["status"] == "offline":
+            report += (" "
+                    + device["hostname"].ljust(18)
+                    + device["ip_address"].ljust(20)
+                    + device["type"].ljust(15)
+                    + location["site"].ljust(15)+ "\n"
+                    )
+            
+report += "\nStatus: WARNING\n"
+
 # Create a summary of the critical data
 summary =""
-summary += "SUMMARY:\n"
+summary += "--------------------------------------------------------------------------------------------------\n"
+summary += "NETWORK REPORT - TechCorp AB\n"
+summary += "--------------------------------------------------------------------------------------------------\n"
+summary += "\n"
+summary += "SUMMARY\n"
+summary += "--------------------------------------------------------------------------------------------------\n"
 
 offline = 0
 for location in data["locations"]:
@@ -17,10 +38,37 @@ for location in data["locations"]:
         if device["status"] == "offline":
             offline += 1
 
-summary += f"⚠ Critical: {offline} devices offline\n"
+summary += f"⚠ Critical: {offline} devices is offline\n"
 
-summary += "⚠ Warning:\n" 
+warning = 0
+for location in data["locations"]:
+    for device in location["devices"]:
+        if device["status"] == "warning":
+            warning += 1
 
+summary += f"⚠ Warning: {warning} devices with warnings\n"
+
+uptime = 0
+for location in data["locations"]:
+    for device in location["devices"]:
+        if device["uptime_days"] < 31:
+            uptime += 1
+
+summary += f"⚠ {uptime} devices with low uptime (<30 days)\n"
+
+port_usage = 0
+for location in data["locations"]:
+    for device in location["devices"]:
+        if "ports" in device:
+            total_ports = device["ports"]["total"]
+            used_ports = device["ports"]["used"]
+            usage_percent = (used_ports / total_ports) * 100
+        
+            if usage_percent > 80:
+                port_usage += 1
+
+summary += f"⚠ {port_usage} switches with high port usage (>80%)\n"
+                        
 # Add summary before main report
 report = summary + report
 
